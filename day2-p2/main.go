ed60925cebdb4336f69ee1ea0b10e6c90f1275cc
Poltergeist <philipph@mesosphere.io>
2019-12-02 12:47:55 +0100

    day2 p 2

---
+++ day2-p2/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	f, err := os.Open("input")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	s := bufio.NewScanner(f)
+	for s.Scan() {
+		input := []int{}
+		for _, number := range strings.Split(s.Text(), ",") {
+			n, _ := strconv.Atoi(number)
+			input = append(input, n)
+		}
+
+		for p1 := 0; p1 < 100; p1++ {
+			for p2 := 0; p2 < 100; p2++ {
+				output := make([]int, len(input))
+				copy(output, input)
+				output[1] = p1
+				output[2] = p2
+				if process(output) == 19690720 {
+					fmt.Println(100*p1 + p2)
+					return
+				}
+			}
+		}
+	}
+}
+
+func process(output []int) int {
+	index := 0
+	for index < len(output) {
+		switch output[index] {
+		case 1:
+			a := output[output[index+1]]
+			b := output[output[index+2]]
+			output[output[index+3]] = a + b
+
+			index += 4
+		case 2:
+			a := output[output[index+1]]
+			b := output[output[index+2]]
+			output[output[index+3]] = a * b
+
+			index += 4
+		case 99:
+			return output[0]
+		default:
+			index++
+		}
+	}
+	return 0
+}
