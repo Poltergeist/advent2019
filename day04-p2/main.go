a5ce641b0991baf8983d3913ea8a04865b88731e
Poltergeist <philipph@mesosphere.io>
2019-12-04 08:49:12 +0100

    day04-p2

---
+++ day04-p2/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	log "github.com/sirupsen/logrus"
+)
+
+func main() {
+	start := 183564
+	end := 657474
+	log.WithFields(log.Fields{"start": start, "end": end}).Info()
+	valid := 0
+
+	for count := start; count <= end; count++ {
+
+		if checkNumber(count) {
+			valid++
+		}
+	}
+
+	log.Infof("There are %d valid numbers.", valid)
+
+}
+
+func checkNumber(count int) bool {
+	last := 0
+	double := make(map[int]string)
+	for _, digit := range fmt.Sprintf("%d", count) {
+		i := normalize(digit)
+		if last > i {
+			return false
+		}
+		if last == i {
+			switch double[i] {
+			case "":
+				double[i] = "yes"
+			case "yes":
+
+				double[i] = "no"
+			}
+		}
+
+		last = i
+	}
+	for _, value := range double {
+		if value == "yes" {
+			return true
+		}
+	}
+	return false
+}
+
+func normalize(digit rune) int {
+	digit = digit - 48
+	return int(digit)
+}
